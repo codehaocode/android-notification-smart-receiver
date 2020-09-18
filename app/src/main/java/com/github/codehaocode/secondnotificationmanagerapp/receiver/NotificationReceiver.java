5ab963447c8ce53cc5faaaeab0726b5cbb8b24c0
Yuhao Zhong <yuhao.zhong@code.berlin>
2020-09-18 12:23:15 +0200

    Declare BroadcastReceiver

---
+++ app/src/main/java/com/github/codehaocode/secondnotificationmanagerapp/receiver/NotificationReceiver.java
@@ -0,0 +1,13 @@
+package com.github.codehaocode.secondnotificationmanagerapp.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
